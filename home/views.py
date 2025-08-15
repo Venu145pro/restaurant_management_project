34fd744925b3402a1234d6c49ce7d4e3d67c85e9
Venu145pro <152844223+Venu145pro@users.noreply.github.com>
2025-08-15 11:57:27 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
+
+from django.conf import settings
 from django.shortcuts import render
 
-# Create your views here.
-from django.conf import settings
+def homepage(request):
+    context = {
+        'restaurant_name': settings.RESTAURANT_NAME
+    }
+    return render(request, 'index.html', context)
