1ff1ec25ef8af2628a73f75e1c4b677ad176ffaf
Venu145pro <152844223+Venu145pro@users.noreply.github.com>
2025-08-22 11:43:08 +0530

    25105

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {"restaurant_name": settings.RESTAURANT_NAME})
